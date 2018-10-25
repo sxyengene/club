ad7c080f65bb3c8109ff3d15faaa884a3b86cdbd
sxyengene <sxyengene@163.com>
2018-10-25 22:02:10 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,25 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    navList: [{
+      title: '评论',
+      bg: 'redbg'
+    }, {
+      title: '评论',
+      bg: 'pinkbg'
+    }],
+  },
+  //事件处理函数
+  bindViewTap: function() {
+
+  },
+  onLoad: function() {
+
+  },
+  getUserInfo: function(e) {
+
+  }
+})
